158d0a9fc473835a5fb1fef6b6a77198039e015a
Julian Marrel <j.marrel@icloud.com>
2021-10-01 11:00:17 +0200

    Post-scriptum

--- src/main/java/org/julianware/utilslib/functional/TriableSupplier.java
+++ src/main/java/org/julianware/utilslib/functional/TriableSupplier.java
@@ -1,8 +1,6 @@
 package org.julianware.utilslib.functional;
 
 
-import java.io.IOException;
-
 /**
  * @author Julian Marrel <julian.marrel@smile.eu>
  * @created 25/06/2021
@@ -10,5 +8,5 @@ import java.io.IOException;
 @FunctionalInterface
 public interface TriableSupplier<T> {
 
-    T giveMeSome() throws IOException;
+    T giveMeSome() throws Exception;
 }
